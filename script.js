fc29edc43b314f0a4e600bee61b5a0fc2ab6aee0
Andrzej Zaborski <andrzejzab@gmail.com>
2019-06-12 22:47:43 +0200

    Snake: snake changing directions on arrows press

--- script.js
+++ script.js
@@ -9,13 +9,10 @@ body.appendChild(canvas)
 
 const ctx = canvas.getContext('2d')
 
-ctx.fillStyle = '#ccc'
-ctx.fillRect(0, 0, 600, 600)
-
-
+document.addEventListener('keydown', changeDirection)
 
 let snake = [{
-        x: 15,
+        x: 45,
         y: 30
     },
     {
@@ -23,15 +20,76 @@ let snake = [{
         y: 30
     },
     {
-        x: 45,
+        x: 15,
         y: 30
     }
 ]
 
-ctx.fillStyle = 'green'
-ctx.strokeStyle = 'black'
+let dx = 15
+let dy = 0
 
-snake.forEach(part => {
-    ctx.fillRect(part.x, part.y, 15, 15)
-    ctx.strokeRect(part.x, part.y, 15, 15)
-})
+
+
+function clearCanvas() {
+    ctx.fillStyle = '#ccc'
+    ctx.fillRect(0, 0, 600, 600)
+}
+
+function drawSnake() {
+    ctx.fillStyle = 'green'
+    ctx.strokeStyle = 'black'
+    snake.forEach(part => {
+        ctx.fillRect(part.x, part.y, 15, 15)
+        ctx.strokeRect(part.x, part.y, 15, 15)
+    })
+}
+
+function moveSnake() {
+    const head = {
+        x: snake[0].x + dx,
+        y: snake[0].y + dy
+    }
+    snake.unshift(head)
+    snake.pop()
+
+}
+
+function changeDirection(event) {
+    const LEFT_KEY = 37
+    const RIGHT_KEY = 39
+    const UP_KEY = 38
+    const DOWN_KEY = 40
+
+    const keyPressed = event.keyCode
+
+    switch (keyPressed) {
+        case LEFT_KEY:
+            dx = -15
+            dy = 0
+            break
+        case RIGHT_KEY:
+            dx = 15
+            dy = 0
+            break
+        case UP_KEY:
+            dx = 0
+            dy = -15
+            break
+        case DOWN_KEY:
+            dx = 0
+            dy = 15
+            break
+        default:
+            break
+    }
+}
+
+clearCanvas()
+drawSnake()
+
+
+setInterval(() => {
+    clearCanvas()
+    moveSnake()
+    drawSnake()
+}, 1000)
