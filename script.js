7cae0ba072acb3ffd89335aa46388f32985c72f8
Andrzej Zaborski <andrzejzab@gmail.com>
2019-06-12 22:16:26 +0200

    Snake: snake starting position added

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ const ctx = canvas.getContext('2d')
 
 ctx.fillStyle = '#ccc'
 ctx.fillRect(0, 0, 600, 600)
+
+
+
+let snake = [{
+        x: 15,
+        y: 30
+    },
+    {
+        x: 30,
+        y: 30
+    },
+    {
+        x: 45,
+        y: 30
+    }
+]
+
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'black'
+
+snake.forEach(part => {
+    ctx.fillRect(part.x, part.y, 15, 15)
+    ctx.strokeRect(part.x, part.y, 15, 15)
+})
